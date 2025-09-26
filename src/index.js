f1af7ce78e4d19ad92f40c4701108a675057ed46
Christopher Penticostes <168892740+Christopher-Penticostes@users.noreply.github.com>
2025-09-26 23:25:24 +0800

    Updated Portfolio

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const redirectPath = window.location.search.replace("?redirect=", "");
+if (redirectPath) {
+  window.history.replaceState(null, "", redirectPath);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename="/Christopher-Portfolio">
